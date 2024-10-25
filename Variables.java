ae58a8adc209442da7f0a2d7761b9264fbf0a8dd
Ayesha Malik <ayeshamalik4a1@gmail.com>
2024-10-25 16:44:55 +0500

    Sum 3 numbers by using variables and int data  type

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+    public static void main(String[] args) {
+        int num1= 5;
+        int num2= 6;
+        int num3 = 4;
+        int sum= num1+num2+num3;
+        System.out.println("The sum of the numbers is: "+sum);
+    }
+    
+}
