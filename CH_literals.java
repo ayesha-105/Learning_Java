dea4427da2e361a0de2c0a665fbab8b8940c1ffd
Ayesha Malik <ayeshamalik4a1@gmail.com>
2024-10-27 10:09:19 +0500

    Learn literals in java

---
+++ CH_literals.java
@@ -0,0 +1,23 @@
+public class CH_literals {
+    public static void main(String[] args) {
+        byte age=34;
+        int age2= 56;
+        short age3= 87;
+        long ageDino= 5666666666666666666L; //right side value is  literal and left side is variable
+        char ch ='A';
+        float f1=5.6f; //5.6 is double by default so we need to typecast it to float
+        double d1=4.66d; 
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(ageDino);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        String name= "Ayesha Malik";
+        System.out.println(name);
+            
+        
+    }
+    
+}
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,14 @@
+public class literals {
+    public static void main(String[] args) {
+        byte age=34;
+        int age2= 56;
+        short age3= 87;
+        long ageDino= 5666666666666666666L; //right side value is  literal and left side is variable
+        char ch ='A';
+        float f1=5.6f; //5.6 is double by default so we need to typecast it to float
+        double d1=4.66d; 
+        System.out.println("age" + "age2" + "age3" + "ageDino");
+        
+    }
+    
+}
