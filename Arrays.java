3f03d7b5532cf5dbca4d7ca68987801c464145e5
Ayesha Malik <ayeshamalik4a1@gmail.com>
2025-01-07 22:33:49 +0500

    Arrays in java

---
+++ Arrays.java
@@ -0,0 +1,20 @@
+public class Arrays {
+    public static void main(String[] args) {
+        /*There are three ways of array declaration 
+        1. declaration then memory allocation like:
+        int [] marks;
+        marks = new int[5];
+        2. declaration + intiallization like:
+        int [] marks = {1,2,3,4,5};*/
+
+        int [] marks = new int[5];  //3 way of array declaration + memory allocation
+        marks[0] = 100;
+        marks[1] = 200;
+        marks[2] = 300;
+        marks[3] = 400;
+        marks[4] = 500;
+        System.out.println(marks[3]);
+        
+         }
+    
+}
