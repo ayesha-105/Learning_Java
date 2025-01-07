a81f350d047d22e754daed5e18e414b32cafb470
Ayesha Malik <ayeshamalik4a1@gmail.com>
2025-01-07 23:01:21 +0500

    Display array with for loop

---
+++ Display_array.java
@@ -0,0 +1,11 @@
+public class Display_array {
+    public static void main(String[] args) {
+        int [] marks = {1,2,3,4,5};
+        //display array by using for loop
+        System.out.println("Display array by using for loop");
+        for(int i=0; i<marks.length; i++){
+            System.out.println(marks[i]);
+        }
+    }
+    
+}
